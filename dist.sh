abfdefc45be9fd414e265154daab5d5f6af72ef4
Malik <malik.hassanaly@gmail.com>
2023-11-21 09:42:59 -0700

    update version

--- dist.sh
+++ dist.sh
@@ -1,3 +1,3 @@
 python -m build
 #python3 -m twine upload --verbose --repository testpypi dist/*
-python3 -m twine upload --verbose --repository pypi dist/*
+python -m twine upload --verbose --repository pypi dist/*
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='prettyPlot',
-    version='0.0.1',
+    version='0.0.2',
     description="Plotting tools for journal quality figures",
     url="https://github.com/malihass/prettyPlot",
     author="Malik Hassanaly",
