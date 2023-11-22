22696ac3e9efafb0eaf36ad7514d30ecfaef6837
Malik <malik.hassanaly@gmail.com>
2023-11-22 13:37:57 -0700

    increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='prettyPlot',
-    version='0.0.6',
+    version='0.0.7',
     description="Plotting tools for journal quality figures",
     url="https://github.com/malihass/prettyPlot",
     author="Malik Hassanaly",
