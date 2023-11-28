bf1f6ded78b43145eae9702e13a724ce0a4e7120
Malik <malik.hassanaly@gmail.com>
2023-11-28 09:31:22 -0700

    upgrade setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='prettyPlot',
-    version='0.0.8',
+    version='0.0.9',
     description="Plotting tools for journal quality figures",
     url="https://github.com/malihass/prettyPlot",
     author="Malik Hassanaly",
