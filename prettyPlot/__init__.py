0c375207f457e66a556409951cff2c26896124ea
Malik <malik.hassanaly@gmail.com>
2024-01-02 14:47:48 -0700

    add version attribute

--- prettyPlot/__init__.py
+++ prettyPlot/__init__.py
@@ -1,4 +1,5 @@
 """Paper plot module"""
 import os
+from prettyPlot.version import __version__
 
 PAPERPLOT_DIR = os.path.dirname(os.path.realpath(__file__))
---
+++ prettyPlot/version.py
@@ -0,0 +1,3 @@
+"""Pretty plot version"""
+
+__version__ = '0.0.14'
